7d2771dca12c7b09ef92807a2e2ed924312f2145
小山由人 <koyama@reytech.co.jp>
2022-10-29 13:58:17 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/com/raisetech/mybatisdemo/MybatisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
